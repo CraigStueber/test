4bf045c0835230f59ebf350196a2a1e595d8ff33
Craig Stueber <cstueber@sauerbrands.com>
2023-12-20 15:06:25 -0500

    supabase work

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { supabase } from "../client";
+
 const Homepage = ({ token }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    const { data: UserProfile, error } = await supabase
+      .from("UserProfile")
+      .select("*");
+    setUsers(UserProfile);
+    console.log(error);
+  }
+  console.log(users);
   return (
     <>
       <h1>Welcome Back {token.user.user_metadata.full_name}</h1>
