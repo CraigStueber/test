9be29bb9d7b12ad4808d47eaec7179a7c3f30eda
CraigStueber <craigstueber@gmail.com>
2023-12-30 17:04:49 -0500

    happenings pulling in from API

--- src/pages/pages/Happenings.jsx
+++ src/pages/pages/Happenings.jsx
@@ -1,12 +1,66 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../../client";
+
 const Happenings = ({ userID }) => {
+  const [happenings, setHappenings] = useState([]);
+  const [locations, setLocations] = useState([]);
+
+  useEffect(() => {
+    const getHappening = async () => {
+      const allHappenings = await fetchHappenings();
+      setHappenings(allHappenings);
+    };
+    const getLoc = async () => {
+      const allLoc = await fetchLocations();
+      setLocations(allLoc);
+    };
+    getHappening();
+    getLoc();
+  }, []);
+
+  async function fetchHappenings() {
+    let { data: Happening, error } = await supabase
+      .from("Happening")
+      .select("*");
+    if (error) {
+      console.error("Error fetching: ", error);
+      return;
+    } else {
+      return Happening;
+    }
+  }
+
+  async function fetchLocations() {
+    let { data: Location, error } = await supabase.from("Location").select("*");
+
+    if (error) {
+      console.error("Error fetching: ", error);
+      return;
+    } else {
+      return Location;
+    }
+  }
   const user = userID.user.id;
-  console.log(user);
+  console.log(locations);
+  console.log(happenings);
+
+  const happeningList = happenings.map((item) => (
+    <div key={item.id}>
+      <h1>{item.name}</h1>
+      <h2>{item.description}</h2>
+      <p>
+        {item.attendees.length} / {item.num_attendees}
+      </p>
+    </div>
+  ));
   return (
     <>
       <h1>Happenings</h1>
-      <h2>{user}</h2>
+      <h2>{happeningList}</h2>
     </>
   );
 };
 
 export default Happenings;
+
+// let { data: Happening, error } = await supabase.from("Happening").select("*");
